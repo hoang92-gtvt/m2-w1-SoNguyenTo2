695c3f548eb5e2918f0bb5c2d47265e67a1bdafb
hoang-gtvt <hoang92.gtvt@gmail.com>
2021-04-04 09:28:38 +0700

    'v2'

--- src/DisplaySNT.java
+++ src/DisplaySNT.java
@@ -9,26 +9,57 @@ public class DisplaySNT {
         int number2= input.nextInt();
         int count=0;
 
-        while(count < number1){
-                boolean check = true;
-                if (number2<2){
-                    check=false;
-                }else if (number2==2 || number2==3){
-                    check=true;
-                }else {
-                    for (int i = 2; i <=Math.sqrt(number2) ; i++) {
-                        if(number2%i==0){
-                            check=false;
-                            break;
-                        }
-                    }
-                }
-                if (check){
-                    System.out.println(number2);
-                    count++;
-                }
+//        while(count < number1){
+//                boolean check = true;
+//                if (number2<2){
+//                    check=false;
+//                }else if (number2==2 || number2==3){
+//                    check=true;
+//                }else {
+//                    for (int i = 2; i <=Math.sqrt(number2) ; i++) {
+//                        if(number2%i==0){
+//                            check=false;
+//                            break;
+//                        }
+//                    }
+//                }
+//                if (check){
+//                    System.out.println(number2);
+//                    count++;
+//                }
+//
+//                number2++;
+//        }
 
-                number2++;
+        while(number2<100){
+
+            checkSNT(number2);
+            number2++;
         }
+
+
+
     }
+    static void checkSNT(int number){
+        boolean check = true;
+        if (number<2){
+            check=false;
+        }else if (number==2 || number==3){
+            check=true;
+        }else {
+            for (int i = 2; i <=Math.sqrt(number) ; i++) {
+                if(number%i==0){
+                    check=false;
+                    break;
+                }
+            }
+        }
+        if (check){
+            System.out.println(number);
+
+        }
+
+    }
+
 }
+

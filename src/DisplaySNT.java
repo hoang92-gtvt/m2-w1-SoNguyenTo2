30a3cae8e9e29df06f65df8d5db9829f8ab43d35
hoang-gtvt <hoang92.gtvt@gmail.com>
2021-04-04 09:13:42 +0700

    'v1'

---
+++ src/DisplaySNT.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class DisplaySNT {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        System.out.println("Nhập số lượng nguyên tố cần in");
+        int number1= input.nextInt();
+        System.out.println("Nhập số bắt đầu cho dãy số");
+        int number2= input.nextInt();
+        int count=0;
+
+        while(count < number1){
+                boolean check = true;
+                if (number2<2){
+                    check=false;
+                }else if (number2==2 || number2==3){
+                    check=true;
+                }else {
+                    for (int i = 2; i <=Math.sqrt(number2) ; i++) {
+                        if(number2%i==0){
+                            check=false;
+                            break;
+                        }
+                    }
+                }
+                if (check){
+                    System.out.println(number2);
+                    count++;
+                }
+
+                number2++;
+        }
+    }
+}
